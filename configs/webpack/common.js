5a3540aa92e7cf68a733a3008bb466a8463b75b8
Nick Bekeris <nick@b3kn.tech>
2019-07-30 23:02:03 -0500

    Updates to configurations
    Removed deprecated configurations
    Implemented cleaner webpack configurations

---
+++ configs/webpack/common.js
@@ -0,0 +1,53 @@
+// shared config (dev and prod)
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    alias: { 'react-dom': '@hot-loader/react-dom' },
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  context: resolve(__dirname, '../../src'),
+  output: {
+    path: resolve(__dirname, '../../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {importLoaders: 1}}
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          {loader: 'css-loader', options: {importLoaders: 1}},
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, '../../src/index.html')
+    })
+  ],
+  performance: {
+    hints: false
+  }
+};
---
+++ configs/webpack/dev.js
@@ -0,0 +1,28 @@
+// development config
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const commonConfig = require('./common');
+const path = require('path');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  entry: [
+    'react-hot-loader/patch', // activate HMR for React
+    'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack/hot/only-dev-server',
+    './index.js' // the entry point of our app
+  ],
+  output: {
+    filename: '[name].[hash].js',
+  },
+  devServer: {
+    hot: true, // enable HMR on the server
+    inline: true,
+    historyApiFallback: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+    new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+  ]
+});
