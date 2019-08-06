cd26ad083299e4c9ac2b9b712f28dec05dbaafa4
Nick Bekeris <nick@b3kn.tech>
2019-08-05 23:25:11 -0500

    Initialization of Services component

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Services';
+
+class Services extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Services</h1>
+        <p>Details to come soon!</p>
+      </div>
+    );
+  }
+}
+
+export default Services;
