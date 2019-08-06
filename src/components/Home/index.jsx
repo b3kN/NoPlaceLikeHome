8dd87cfc590a088e89a9462f2e493d523b356934
Nick Bekeris <nick@b3kn.tech>
2019-08-05 23:18:52 -0500

    Replaced Welcome with Home component

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Home';
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Details to come soon!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/components/Welcome/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Welcome = () => (
-  <p>Welcome to the playground for No Place Like Home Pet Sitting</p>
-);
-
-export default Welcome;
