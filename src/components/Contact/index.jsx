edd63900a5924d642d2129e8e449e0a38f0c0786
Nick Bekeris <nick@b3kn.tech>
2019-08-05 23:23:48 -0500

    Initialization of Contact component

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Contact';
+
+class Contact extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <p>Details to come soon!</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
