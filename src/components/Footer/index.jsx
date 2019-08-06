13fe6ff3254e1f6d8ca6a5ef0460ead37562fb66
Nick Bekeris <nick@b3kn.tech>
2019-08-05 23:24:40 -0500

    Initialization of Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Footer';
+
+const Footer = () => (
+  <footer>
+    <div>
+      <span className="bold">
+        Footer Placeholder
+      </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
