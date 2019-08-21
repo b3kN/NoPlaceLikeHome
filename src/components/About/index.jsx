a2e79571a4d0c0705718ba1d55ba81ecd7008096
Nick Bekeris <nick@b3kn.tech>
2019-08-20 23:02:11 -0500

    Update simple content components
    Change simple content components to stateless components

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import './About';
 
-class About extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <h1>About</h1>
-        <p>Details to come soon!</p>
-      </div>
-    );
-  }
-}
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>Details to come soon!</p>
+  </div>
+);
 
 export default About;
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import './Services';
 
-class Services extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <h1>Services</h1>
-        <p>Details to come soon!</p>
-      </div>
-    );
-  }
-}
+const Services = () => (
+  <div>
+    <h1>Services</h1>
+    <p>Details to come soon!</p>
+  </div>
+);
 
 export default Services;
