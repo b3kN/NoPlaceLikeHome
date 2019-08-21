af5669b7cac0835deafea2c9e88b4c24da232d37
Nick Bekeris <nick@b3kn.tech>
2019-08-20 23:11:41 -0500

    Improvements to header
    Updated styles to allow for sticky-scrolling
    Updated class to React.Component
    Implemented logic for navigation bar updates

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,32 +3,78 @@ import { NavLink } from 'react-router-dom';
 
 import './Header';
 
-const Header = (props) => (
-  <header>
-    <title>
-      <NavLink className="verticalAlign" to="/">
-        {props.title}
-      </NavLink>
-    </title>
-    <div className="navigation">
-      <NavLink exact to="/" activeClassName="active">
-        Home
-          <span />
-      </NavLink>
-      <NavLink exact to="/about" activeClassName="active">
-        About
-          <span />
-      </NavLink>
-      <NavLink exact to="/services" activeClassName="active">
-        Services
-          <span />
-      </NavLink>
-      <NavLink exact to="/contact" activeClassName="active">
-        Contact
-          <span />
-      </NavLink>
-    </div>
-  </header>
-);
+let bannerHeight = 0,
+  bannerChanged = false;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+    bannerHeight = 0;
+  }
+
+  handleScroll() {
+    // TODO: Set up proper React functionality here
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+      checkHeight = bannerHeight > 0 ? bannerHeight : document.getElementById("headerBanner").offsetHeight - 60,
+      header = document.getElementById("headerWrapper"),
+      content = document.getElementById("contentWrapper");
+
+    bannerHeight = bannerHeight === 0 ? checkHeight : bannerHeight;
+
+    if (bannerChanged) {
+      bannerChanged = false;
+    } else {
+      if (winScroll > checkHeight) {
+        header.classList.add("scrolled");
+        content.classList.add("scrolled");
+        bannerChanged = true;
+      } else {
+        header.classList.remove("scrolled");
+        content.classList.remove("scrolled");
+      }
+    }
+  }
+
+  render() {
+    return (
+      <header id="headerWrapper">
+        <div id="headerBanner" className="banner" />
+        <div className="navHolder">
+          <title>
+            <NavLink className="verticalAlign" to="/">
+              {this.props.title}
+            </NavLink>
+          </title>
+          <div className="navigation">
+            <NavLink exact to="/" activeClassName="active">
+              Home
+            <span />
+            </NavLink>
+            <NavLink exact to="/about" activeClassName="active">
+              About
+            <span />
+            </NavLink>
+            <NavLink exact to="/services" activeClassName="active">
+              Services
+            <span />
+            </NavLink>
+            <NavLink exact to="/contact" activeClassName="active">
+              Contact
+            <span />
+            </NavLink>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Header;
