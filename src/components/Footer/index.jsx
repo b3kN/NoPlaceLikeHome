f4cb2c4d460f1eed241bff80dcf03c18f9a450f0
Nick Bekeris <nick@b3kn.tech>
2019-08-26 23:31:17 -0500

    Updates to Footer component
    Updated component to utilize full React features
    New function to determine if sticky footer is needed
    Updated UI to apply class as state is set

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,15 +2,41 @@ import React from "react";
 
 import './Footer';
 
-const Footer = () => (
-  <footer>
-    <span className="topper" />
-    <div>
-      <span className="bold">
-        Footer Placeholder
-      </span>
-    </div>
-  </footer>
-);
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stickyFooter: ""
+    };
+    this.toggleStickyFooter = this.toggleStickyFooter.bind(this);
+  }
+
+  componentDidMount() {
+    this.toggleStickyFooter();
+  }
+
+  toggleStickyFooter() {
+    let headerHeight = document.getElementById("headerWrapper").clientHeight,
+      contentHeight = document.getElementById("contentWrapper").clientHeight,
+      rootHeight = document.getElementById("root").clientHeight;
+
+    this.setState({
+      stickyFooter: contentHeight < rootHeight - headerHeight ? "sticky" : ""
+    });
+  }
+
+  render() {
+    return (
+      <footer className={this.state.stickyFooter} >
+        <span className="topper" />
+        <div>
+          <span className="bold">
+            Footer Placeholder
+          </span>
+        </div>
+      </footer>
+    );
+  }
+}
 
 export default Footer;
