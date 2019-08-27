fe4370090557cd411140cbf1381ee5be7f2351da
Nick Bekeris <nick@b3kn.tech>
2019-08-26 23:32:21 -0500

    Updates for content displays
    Created temporary verbiage on Home component
    Updated Content styles to match expected UX

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,37 +4,14 @@ import './Home';
 
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <h1>No Place Like Home</h1>
+    <h3>
+      Thank you for visiting the No Place Like Home in Marion, Iowa website!
+    </h3>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut iaculis lorem, ut pulvinar purus. Donec non ex nisi. Donec sed pretium mi, ac fermentum eros. Vivamus luctus, sapien ac efficitur rhoncus, velit nisi gravida dui, ut dictum magna dolor at ligula. Fusce lobortis, purus gravida consectetur dictum, risus nibh lobortis lectus, consequat euismod libero velit ac nibh. Donec maximus est ut sem finibus, ut dignissim velit pretium. Sed lobortis nunc ac condimentum tristique. Aliquam in elit lectus.
-        </p>
-    <p>
-      Mauris faucibus facilisis nunc sit amet egestas. Nullam viverra metus non auctor tempor. In vitae magna dolor. Duis lobortis egestas sodales. Nam lobortis lectus vel mauris malesuada venenatis. Morbi consectetur erat eget augue rhoncus, sit amet interdum libero tincidunt. In eget turpis risus. Aliquam ultrices rhoncus nunc eu consectetur. Sed sit amet erat nibh. Donec non lectus elit. Sed laoreet rutrum quam, non faucibus nunc pulvinar non.
-        </p>
-    <p>
-      Nulla aliquam vulputate diam, eget molestie risus laoreet a. Duis a quam convallis, sollicitudin dolor ac, luctus massa. Phasellus tempus diam volutpat quam eleifend, quis placerat massa vulputate. Duis erat dui, laoreet non dolor eget, hendrerit rhoncus lectus. In viverra tellus dolor, quis venenatis leo lobortis id. Quisque a gravida eros. Aenean commodo tellus eu diam consectetur sollicitudin. Etiam scelerisque eleifend augue a venenatis. Fusce ullamcorper ultrices dolor, nec laoreet purus. Praesent fermentum euismod facilisis. Nulla vulputate nisl sed lorem bibendum, quis finibus tellus lobortis. Nulla nisl nisl, accumsan dapibus condimentum at, tempor eu justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque commodo tincidunt enim eu pharetra. Sed vulputate hendrerit nibh, id bibendum risus vulputate vel.
-        </p>
-    <p>
-      In ullamcorper venenatis odio a consequat. Vivamus varius odio velit, et ullamcorper purus euismod ac. Donec eget laoreet est. Mauris iaculis porttitor erat ut mattis. In porttitor luctus arcu, eget gravida neque faucibus in. Maecenas dolor libero, gravida sit amet nulla a, elementum cursus nisl. Suspendisse in varius erat. Sed aliquam nulla sed commodo convallis. Quisque sit amet enim eget diam vulputate convallis. Suspendisse pretium pretium neque, non tincidunt eros pellentesque a. Maecenas cursus dolor ut molestie commodo. Integer pharetra ac lorem non rutrum.
-        </p>
-    <p>
-      Proin suscipit semper libero eget dapibus. Phasellus sit amet libero rutrum, auctor magna et, pharetra augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non nunc tellus. Aenean imperdiet neque in massa dictum, vel sollicitudin nisl efficitur. Cras id justo ullamcorper, ullamcorper magna sit amet, lacinia massa. Sed sollicitudin tellus et mauris consequat, id vehicula nunc semper. Vestibulum eget egestas quam.
-        </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut iaculis lorem, ut pulvinar purus. Donec non ex nisi. Donec sed pretium mi, ac fermentum eros. Vivamus luctus, sapien ac efficitur rhoncus, velit nisi gravida dui, ut dictum magna dolor at ligula. Fusce lobortis, purus gravida consectetur dictum, risus nibh lobortis lectus, consequat euismod libero velit ac nibh. Donec maximus est ut sem finibus, ut dignissim velit pretium. Sed lobortis nunc ac condimentum tristique. Aliquam in elit lectus.
-        </p>
-    <p>
-      Mauris faucibus facilisis nunc sit amet egestas. Nullam viverra metus non auctor tempor. In vitae magna dolor. Duis lobortis egestas sodales. Nam lobortis lectus vel mauris malesuada venenatis. Morbi consectetur erat eget augue rhoncus, sit amet interdum libero tincidunt. In eget turpis risus. Aliquam ultrices rhoncus nunc eu consectetur. Sed sit amet erat nibh. Donec non lectus elit. Sed laoreet rutrum quam, non faucibus nunc pulvinar non.
-        </p>
-    <p>
-      Nulla aliquam vulputate diam, eget molestie risus laoreet a. Duis a quam convallis, sollicitudin dolor ac, luctus massa. Phasellus tempus diam volutpat quam eleifend, quis placerat massa vulputate. Duis erat dui, laoreet non dolor eget, hendrerit rhoncus lectus. In viverra tellus dolor, quis venenatis leo lobortis id. Quisque a gravida eros. Aenean commodo tellus eu diam consectetur sollicitudin. Etiam scelerisque eleifend augue a venenatis. Fusce ullamcorper ultrices dolor, nec laoreet purus. Praesent fermentum euismod facilisis. Nulla vulputate nisl sed lorem bibendum, quis finibus tellus lobortis. Nulla nisl nisl, accumsan dapibus condimentum at, tempor eu justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque commodo tincidunt enim eu pharetra. Sed vulputate hendrerit nibh, id bibendum risus vulputate vel.
-        </p>
-    <p>
-      In ullamcorper venenatis odio a consequat. Vivamus varius odio velit, et ullamcorper purus euismod ac. Donec eget laoreet est. Mauris iaculis porttitor erat ut mattis. In porttitor luctus arcu, eget gravida neque faucibus in. Maecenas dolor libero, gravida sit amet nulla a, elementum cursus nisl. Suspendisse in varius erat. Sed aliquam nulla sed commodo convallis. Quisque sit amet enim eget diam vulputate convallis. Suspendisse pretium pretium neque, non tincidunt eros pellentesque a. Maecenas cursus dolor ut molestie commodo. Integer pharetra ac lorem non rutrum.
-        </p>
-    <p>
-      Proin suscipit semper libero eget dapibus. Phasellus sit amet libero rutrum, auctor magna et, pharetra augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non nunc tellus. Aenean imperdiet neque in massa dictum, vel sollicitudin nisl efficitur. Cras id justo ullamcorper, ullamcorper magna sit amet, lacinia massa. Sed sollicitudin tellus et mauris consequat, id vehicula nunc semper. Vestibulum eget egestas quam.
-        </p>
+      No Place Like Home offers many different services and delightful appeal with the family members involved.
+      With every visit we have with your pets are sure to be special as we enjoy the moments just as much, without a doubt!
+    </p>
   </div>
 );
 
