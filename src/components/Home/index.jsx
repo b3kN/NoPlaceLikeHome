7715dd08a4ec9f06373c98162b9d0e3dadec9847
Nick Bekeris <nick@b3kn.tech>
2019-09-09 23:56:34 -0500

    Updates for images in trip sections
    + Added icons  for trip sections in Home Component
    + Added updated fonts to trips and content sections

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -12,30 +12,57 @@ const Home = () => (
       <p>
         No Place Like Home offers many different services and delightful appeal with the family members involved.
         With every visit we have with your pets are sure to be special as we enjoy the moments just as much, without a doubt!
+        I always am excited and enjoy meeting new families and connect with each member. 
+      </p>
+
+      <p>
+        With No Place Like Home, I try to make each experience as little as different for the loved pets in families for any occasion.
+        There are many services that come in addition the offered petsitting services... You can find more about these below.
       </p>
     </div>
     <div className="trips-full">
       <div className="trips-holder">
         <div>
+          <span className="img-background home" />
+
           <span>
-            Trip Section 1
+            Just like home
           </span>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales id sem nec pharetra. 
         </div>
         <div>
+          <span className="img-background heart" />
+
           <span>
-            Trip Section 2
+            Care & devotion
           </span>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales id sem nec pharetra. Nunc auctor dui nulla, ac ultricies ligula sollicitudin vitae. 
           Pellentesque vestibulum blandit nisl, a pretium magna mattis ac. Nullam diam sapien, mollis vitae elit commodo, sodales fermentum enim.
         </div>
         <div>
+          <span className="img-background camera" />
+
           <span>
-            Trip Section 3
+            Unique care & connection
           </span>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales id sem nec pharetra. Nunc auctor dui nulla, ac ultricies ligula sollicitudin vitae.
         </div>
       </div>
+    </div>
+    <div className="container">
+      <h3>
+        Stay in touch!
+      </h3>
+      <p>
+        No Place Like Home offers many different services and delightful appeal with the family members involved.
+        With every visit we have with your pets are sure to be special as we enjoy the moments just as much, without a doubt!
+        I always am excited and enjoy meeting new families and connect with each member.
+      </p>
+
+      <p>
+        With No Place Like Home, I try to make each experience as little as different for the loved pets in families for any occasion.
+        There are many services that come in addition the offered petsitting services... You can find more about these below.
+      </p>
     </div>
   </div>
 );
