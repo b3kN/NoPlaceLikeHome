07d5eda5d7a43b410df08e9b1c89abd6d4503a45
Nick Bekeris <nick@b3kn.tech>
2019-09-22 23:25:59 -0500

    Basic updates for About page
    + Moved about page from components to pages directory
    + Implemented basic about the owner details
    + TODO: Implement pictures and additional flair

---
+++ client/src/pages/About/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './About';
+
+const About = () => (
+  <div>
+    <div className="container">
+      <h1>About Us</h1>
+      <h3>
+        Priscilla Ring
+      </h3>
+      <p>
+        I work as the main pet sitter with No Place Like Home and run the show for giving all furry family members what they desire and love during our adventures.
+        As a pet lover myself, I find several joys in the companionship built with every animal and cherish their presence in our lives greatly.
+        As a pet parent, with a deep, lifelong love of amimals, especially dogs and cats, I prioritize keeping my own furbabies healthy, happy and safe. 
+        I bring this same sense of loving responsibility to my clients and their pets.
+      </p>
+
+      <p>
+        I love to care for your animals in the way you want, but with an extra personal touch as if they were my own pets. 
+        The faces and smiles of your pets are all the more important for you to know they are still being loved when far away, and I love to save those moments with pictures and experiences for everyone to enjoy... 
+        no matter where they are, everyone will feel right at home.
+      </p>
+    </div>
+  </div>
+);
+
+export default About;
